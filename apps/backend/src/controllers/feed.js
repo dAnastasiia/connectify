66aaec75f3f62904f6d4803154ea9ccbd832c325
Anastasiia Drozd <a.drozd@spd-ukraine.com>
2024-04-02 21:13:32 +0300

    BE: add create post emulator with data validation

--- apps/backend/src/controllers/feed.js
+++ apps/backend/src/controllers/feed.js
@@ -1,4 +1,6 @@
+import { randomUUID } from 'crypto';
 import { DateTime } from 'luxon';
+import { validationResult } from 'express-validator';
 
 export const getPosts = (req, res, next) => {
   res.status(200).json({
@@ -10,17 +12,37 @@ export const getPosts = (req, res, next) => {
         content:
           'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum perspiciatis a repellat quidem corrupti non, est quisquam? Sunt vel eum labore, beatae, totam quia iusto odit nulla quibusdam dignissimos doloribus tempore voluptatibus unde dolor dolore facilis maxime rerum, voluptates deserunt sapiente fugiat soluta velit distinctio. Quasi fuga eos ullam nisi.',
         imageUrl: 'images/bicycle.jpg',
-        createdAt: DateTime.now(),
+        createdAt: '2024-04-01T19:04:15.729+03:00',
       },
     ],
   });
 };
 
 export const createPost = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log('errors: ', errors);
+    res.status(422).json({
+      message: 'Data is incorrect',
+      errors: errors.array(),
+    });
+  }
+
   const { title, content } = req.body;
+
+  const data = {
+    _id: randomUUID(),
+    title,
+    content,
+    author: 'J. K. Rowling',
+    imageUrl: 'images/bicycle.jpg',
+    createdAt: DateTime.now().toISO(),
+  };
+  console.log('BE data: ', data);
 
   res.status(201).json({
     message: 'Successfully created',
-    post: { id: Date.now(), title, content },
+    data,
   });
 };
