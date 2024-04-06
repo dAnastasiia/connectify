7e8b637141d2e81a8dee0e1558f024874279a862
Anastasiia Drozd <a.drozd@spd-ukraine.com>
2024-04-06 16:15:09 +0300

    BE: add JWT and auth middleware

--- apps/backend/src/routes/feed.js
+++ apps/backend/src/routes/feed.js
@@ -9,14 +9,17 @@ import {
   deletePost,
 } from '../controllers/feed';
 
+import isAuth from '../middlewares/auth';
+
 const router = Router();
 
 // GET posts
-router.get('/posts', getPosts);
+router.get('/posts', isAuth, getPosts);
 
 // CREATE post
 router.post(
   '/post',
+  isAuth,
   [
     body('title', 'Invalid title').trim().isString().isLength({ min: 5 }),
     body('content', 'Invalid content').trim().isString().isLength({ min: 5 }),
@@ -27,6 +30,7 @@ router.post(
 // UPDATE post
 router.put(
   '/post/:postId',
+  isAuth,
   [
     body('title', 'Invalid title').trim().isString().isLength({ min: 5 }),
     body('content', 'Invalid content').trim().isString().isLength({ min: 5 }),
@@ -35,9 +39,9 @@ router.put(
 );
 
 // DELETE post
-router.delete('/post/:postId', deletePost);
+router.delete('/post/:postId', isAuth, deletePost);
 
 // READ post
-router.get('/post/:postId', getPost);
+router.get('/post/:postId', isAuth, getPost);
 
 export default router;
