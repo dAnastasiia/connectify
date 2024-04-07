c484c3bdfe1ac1ae78c1c661d098300cd175b9ff
Anastasiia Drozd <a.drozd@spd-ukraine.com>
2024-04-07 13:52:50 +0300

    remove extra settings

--- apps/frontend/src/app/providers/QueryClientProvider/queryClient.ts
+++ apps/frontend/src/app/providers/QueryClientProvider/queryClient.ts
@@ -1,10 +1,3 @@
 import { QueryClient } from '@tanstack/react-query';
 
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-      refetchOnWindowFocus: false,
-    },
-  },
-});
+export const queryClient = new QueryClient();
