7716fcbece9639680227d524aebdb3c32e07b3f2
Anastasiia Drozd <a.drozd@spd-ukraine.com>
2024-04-08 11:11:14 +0300

    FE: add useGraphQL hook

---
+++ apps/frontend-graphql/src/app/hooks/useGraphQL/index.ts
@@ -0,0 +1,47 @@
+import { ClientError } from 'graphql-request';
+import { useEffect } from 'react';
+
+import { useMutation } from '@tanstack/react-query';
+
+import { CustomError } from '@frontend-graphql/types';
+
+import { handleGenericError, handleGraphQLErrors } from './utils';
+
+interface UseGraphQLProps<T, P> {
+  mutationFn: (data: T) => Promise<P>;
+  onSuccess: (data: P) => void;
+  onError: (errors: CustomError[]) => void;
+  mutationKey?: unknown[];
+}
+
+export default function useGraphQL<T, P>({
+  onSuccess,
+  onError,
+  mutationFn,
+  mutationKey,
+}: UseGraphQLProps<T, P>) {
+  const { mutate, isPending, isSuccess, data, error } = useMutation({
+    mutationFn,
+    mutationKey,
+  });
+
+  useEffect(() => {
+    if (data && isSuccess) {
+      onSuccess(data);
+    }
+  }, [isSuccess, onSuccess, data]);
+
+  useEffect(() => {
+    if (error) {
+      const errors = (error as ClientError).response.errors;
+
+      if (errors?.length) {
+        onError(handleGraphQLErrors(errors));
+      } else {
+        onError(handleGenericError());
+      }
+    }
+  }, [error, onError]);
+
+  return { mutate, isPending };
+}
